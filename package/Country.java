04c64c7fbedb29c495adfea9799c775fb1c482d8
Sajid9016 <150129904+Sajid9016@users.noreply.github.com>
2024-05-29 12:45:07 +0530

    Add files via upload

--- package/Country.java
+++ package/Country.java
@@ -1,1 +1,18 @@
+package packej;
 
+public class Country {
+	private String cname;
+
+	public Country(String cname) {
+		
+		this.cname = cname;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Country [cname=" + cname + "]";
+	}
+	
+	
+}
